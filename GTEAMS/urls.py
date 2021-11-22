b8dd648e1236d7da50ac4f5ea0032540aef63a92
“QuynhGiang0505” <quynhgiang050501@gmail.com>
2021-11-22 18:09:34 +0700

    update

--- GTEAMS/urls.py
+++ GTEAMS/urls.py
@@ -5,7 +5,7 @@ from GTEAMS_APP import views
 from django.conf.urls import url, handler404, handler500
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("GTEAMS_APP.urls")),
+    path('GTEAMS/', include("GTEAMS_APP.urls")),
     path('accounts/',include("accounts.urls"))
 ]
 handler404 = 'GTEAMS_APP.views.error'
