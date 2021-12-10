def798af20f82899a3a404e63c889ae4a4a61962
NguyenMyQuynh <19520241@gm.uit.edu.vn>
2021-12-10 22:17:19 +0700

    remove check available email huhu

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,8 +8,8 @@ class CreateUserForm(UserCreationForm):
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
 
-	def clean(seft):
-		cleaned_data =super().clean()
-		email=seft.cleaned_data.get('email')
-		if User.objects.filter(email=email).exists():
-				raise forms.ValidationError('Email is already exists!')
+	# def clean(seft):
+	# 	cleaned_data =super().clean()
+	# 	email=seft.cleaned_data.get('email')
+	# 	if User.objects.filter(email=email).exists():
+	# 			raise forms.ValidationError('Email is already exists!')
