d3250289e97dc0565a5a1aca746a0923562cac02
Lam Thanh Ngan <60861471+LamThanhNgan@users.noreply.github.com>
2021-12-13 07:51:06 +0700

    Update urls.py

--- GTEAMS/urls.py
+++ GTEAMS/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from GTEAMS_APP import views
 from django.conf.urls import url, handler404, handler500
-from django.conf import settings
-from django.views.static import serve
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,7 +12,5 @@ urlpatterns = [
     path('accounts/',include("accounts.urls")),
     path('accounts/', include('allauth.urls')),
     path('tinymce/',include('tinymce.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ]
